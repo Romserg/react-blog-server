34d185b461d54b474d352f3c00af178f79696bc2
Romserg <serg.romankov@gmail.com>
2021-09-16 00:47:09 +0300

    Upvote articles logic

--- src/server.js
+++ src/server.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+const articlesInfo = {
+  'learn-react': {
+    upvotes: 0,
+  },
+  'learn-node': {
+    upvotes: 0,
+  },
+  'my-thoughts-on-resume': {
+    upvotes: 0,
+  },
+};
 
 const app = express();
 
-app.get('/hello', (req, res) => {
-  return res.send('Hello');
+app.use(bodyParser.json());
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+  articlesInfo[articleName].upvotes += 1;
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes}`);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
